276a6a44db01e83eed9f5b6e458f7232cc69fff1
Edwin.Lim <elim@sapient.com>
2019-03-21 14:35:13 +0800

    - init commit

--- scripts/base/configs.js
+++ scripts/base/configs.js
@@ -1,10 +1,10 @@
 export const otherConfig = {};
 
 export const firebaseConfig = {
-  apiKey: 'AIzaSyBFUPHfUhnO4nP7P5D5RUQkgkT7U2cAPE0',
-  authDomain: 'seats-reservation.firebaseapp.com',
-  databaseURL: 'https://seats-reservation.firebaseio.com',
-  projectId: 'seats-reservation',
-  storageBucket: 'seats-reservation.appspot.com',
-  messagingSenderId: '986190968286',
+  apiKey: 'XXXXXXXXX',
+  authDomain: 'XXXXXXXXX',
+  databaseURL: 'XXXXXXXXX',
+  projectId: 'XXXXXXXXX',
+  storageBucket: 'XXXXXXXXX',
+  messagingSenderId: 'XXXXXXXXX',
 };
